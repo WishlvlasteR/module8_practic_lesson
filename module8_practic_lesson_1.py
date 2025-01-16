64adc71c1ed384b69bc05749bedc903a4f3522d1
Ruslan <wishiviaster99@gmail.com>
2025-01-16 14:46:17 +1200

    Add files via upload

---
+++ module8_practic_lesson_1.py
@@ -0,0 +1,13 @@
+def add_everything_up(a, b):
+    try:
+        result = a + b
+
+    except TypeError:
+        return f'{a}' + f'{b}'
+    else:
+        return f'{result:.3f}'
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
